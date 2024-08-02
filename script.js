6122de94698330c17d98a83322abe4bc3921a170
DanielRuizV <98991633+DanielRuizV@users.noreply.github.com>
2024-08-02 14:35:37 -0600

    script.js

--- script.js
+++ script.js
@@ -17,21 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const tableBody = document.querySelector('tbody');
     const searchInput = document.getElementById('search');
-    const tableHeaders = document.querySelectorAll('th');
 
-   function parseCSV(data) {
-    const rows = data.split('\n').filter(row => row.trim() !== ''); // Filtrar filas vacías
-    const headers = rows[0].split(',').map(header => header.trim()); // Asegúrate de trim los encabezados
+    // Function to parse CSV data into an array of objects
+    function parseCSV(data) {
+        const rows = data.split('\n').filter(row => row.trim() !== ''); // Filtrar filas vacías
+        const headers = rows[0].split(',').map(header => header.trim()); // Asegúrate de trim los encabezados
 
-    return rows.slice(1).map(row => {
-        const values = row.split(',');
-        return headers.reduce((object, header, index) => {
-            object[header] = values[index] ? values[index].trim() : ''; // Verificación de valores indefinidos
-            return object;
-        }, {});
-    });
-}
-
+        return rows.slice(1).map(row => {
+            const values = row.split(',');
+            return headers.reduce((object, header, index) => {
+                object[header] = values[index] ? values[index].trim() : ''; // Verificación de valores indefinidos
+                return object;
+            }, {});
+        });
+    }
 
     // Function to load the data into the table
     function loadTableData(casos) {
@@ -40,52 +39,51 @@ document.addEventListener('DOMContentLoaded', function() {
         casos.forEach((caso, index) => {
             const row = document.createElement('tr');
             row.innerHTML = `
-                <td>${index}</td>
-                <td>${caso['NÚMERO']}</td>
-                <td>${caso['ABOGADO']}</td>
-                <td>${caso['CASO']}</td>
-                <td>${caso['DEMANDA PRESENTADA']}</td>
-                <td>${caso['MEDIACIÓN']}</td>
-                <td>${caso['55 LESIONADO/51 FINADO']}</td>
-                <td>${caso['FECHA DE PRESENTACIÓN']}</td>
-                <td>${caso['ADMISIÓN']}</td>
-                <td>${caso['FECHA DE ADMISIÓN']}</td>
+                <td>${caso['NUMERO'] || (index + 1)}</td>
+                <td>${caso['ABOGADO'] || 'No definido'}</td>
+                <td>${caso['CASO'] || 'No definido'}</td>
+                <td>${caso['DEMANDA PRESENTADA'] || 'No definido'}</td>
+                <td>${caso['MEDIACION'] || 'No definido'}</td>
+                <td>${caso['55 LESIONADO/51 FINADO'] || 'No definido'}</td>
+                <td>${caso['FECHA DE PRESENTACIÓN'] || 'No definido'}</td>
+                <td>${caso['ADMISION']}</td>
+                <td>${caso['FECHA DE ADMISION']}</td>
                 <td>${caso['ASEGURADORA']}</td>
                 <td>${caso['ASEGURADORA EMPLAZADA']}</td>
                 <td>${caso['FECHA DE EMPLAZAMIENTO ASEGURADORA']}</td>
                 <td>${caso['VISTA ASEGURADORA']}</td>
                 <td>${caso['FECHA DESAHOGO DE VISTA ASEGURADORA']}</td>
-                <td>${caso['DIRECCIÓN ASEGURADORA']}</td>
-                <td>${caso['VENCIMIENTO CONTESTACIÓN ASEGURADORA']}</td>
-                <td>${caso['DÍAS RESTANTES']}</td>
+                <td>${caso['DIRECCION ASEGURADORA']}</td>
+                <td>${caso['VENCIMIENTO CONTESTACION ASEGURADORA']}</td>
+                <td>${caso['DIAS RESTANTES']}</td>
                 <td>${caso['VENCIMIENTO CONTESTACIÓN VISTA ASEGURADORA']}</td>
-                <td>${caso['DÍAS RESTANTES']}</td>
+                <td>${caso['DIAS RESTANTES']}</td>
                 <td>${caso['PROPIETARIO A EMPLAZAR']}</td>
                 <td>${caso['PROPIETARIO EMPLAZADO']}</td>
                 <td>${caso['FECHA DE EMPLAZAMIENTO PROPIETARIO']}</td>
                 <td>${caso['VISTA PROPIETARIO']}</td>
                 <td>${caso['FECHA DE DESAHOGO DE VISTA PROPIETARIO']}</td>
-                <td>${caso['DIRECCIÓN PROPIETARIO']}</td>
+                <td>${caso['DIRECCION PROPIETARIO']}</td>
                 <td>${caso['CONTACTO DE PROPIETARIO']}</td>
-                <td>${caso['VENCIMIENTO CONTESTACIÓN ASEGURADORA']}</td>
-                <td>${caso['DÍAS RESTANTES']}</td>
-                <td>${caso['VENCIMIENTO CONTESTACIÓN DESAHOGO DE VISTA ASEGURADORA']}</td>
-                <td>${caso['DÍAS RESTANTES']}</td>
+                <td>${caso['VENCIMIENTO CONTESTACION ASEGURADORA']}</td>
+                <td>${caso['DIAS RESTANTES']}</td>
+                <td>${caso['VENCIMIENTO CONTESTACION DESAHOGO DE VISTA ASEGURADORA']}</td>
+                <td>${caso['DIAS RESTANTES']}</td>
                 <td>${caso['CONDUCTOR']}</td>
                 <td>${caso['CONDUCTOR EMPLAZADO']}</td>
                 <td>${caso['FECHA EMPLAZAMIENTO CONDUCTOR']}</td>
                 <td>${caso['VISTA CONDUCTOR']}</td>
                 <td>${caso['FECHA DE DESAHOGO VISTA CONDUCTOR']}</td>
-                <td>${caso['DIRECCIÓN DE RESPONSABLE']}</td>
+                <td>${caso['DIRECCION DE RESPONSABLE']}</td>
                 <td>${caso['CONTACTO DE RESPONSABLE']}</td>
-                <td>${caso['VENCIMIENTO CONTESTACIÓN']}</td>
-                <td>${caso['DÍAS RESTANTES']}</td>
+                <td>${caso['VENCIMIENTO CONTESTACION']}</td>
+                <td>${caso['DIAS RESTANTES']}</td>
                 <td>${caso['VENCIMIENTO DESAHOGO DE VISTA']}</td>
-                <td>${caso['DÍAS RESTANTES']}</td>
+                <td>${caso['DIAS RESTANTES']}</td>
                 <td>${caso['COMENTARIO']}</td>
-                <td>${caso['FECHA ÚLTIMA ACTUACIÓN']}</td>
+                <td>${caso['FECHA ULTIMA ACTUACION']}</td>
                 <td>${caso['FECHA DE VENCIMIENTO']}</td>
-                <td>${caso['DÍAS RESTANTES']}</td>
+                <td>${caso['DIAS RESTANTES']}</td>
                 <td>${caso['DEMANDA PRESENTADA']}</td>
                 <td>${caso['EXHORTO']}</td>
                 <td>${caso['REPRESENTADOS DEMANDA']}</td>
@@ -95,9 +93,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 <td>${caso['JUZGADO']}</td>
                 <td>${caso['EXPEDIENTE']}</td>
                 <td>${caso['TIPO DE AUDIENCIA']}</td>
-                <td>${caso['ABOGADO QUE DESAHOGARÁ']}</td>
+                <td>${caso['ABOGADO QUE DESAHOGARA']}</td>
                 <td>${caso['LINK DE AUDIENCIA']}</td>
-                <td>${caso['PSICOLOGÍA']}</td>
+                <td>${caso['PSICOLOGIA']}</td>
                 <td>${caso['TRABAJO SOCIAL']}</td>
                 <td>${caso['MEDICINA']}</td>
                 <td>${caso['HECHOS VIALES']}</td>
@@ -105,22 +103,22 @@ document.addEventListener('DOMContentLoaded', function() {
                 <td>${caso['LINK DE CARPETA']}</td>
                 <td>${caso['NOTA']}</td>
                 <td>${caso['PUBLICAR']}</td>
-                <td>${caso['PÓLIZA']}</td>
+                <td>${caso['POLIZA']}</td>
                 <td>${caso['MONTO']}</td>
                 <td>${caso['FECHA DE LOS HECHOS']}</td>
                 <td>${caso['LUGAR DEL SINIESTRO']}</td>
                 <td>${caso['NOTA']}</td>
                 <td>${caso['CONCLUYO']}</td>
-                <td>${caso['PÓLIZA']}</td>
+                <td>${caso['POLIZA']}</td>
                 <td>${caso['VIGENCIA DE LA PÓLIZA']}</td>
                 <td>${caso['AGENTE']}</td>
                 <td>${caso['DOMICILIO']}</td>
-                <td>${caso['TELÉFONO']}</td>
+                <td>${caso['TELEFONO']}</td>
                 <td>${caso['CORREO ELECTRÓNICO DEL AGENTE']}</td>
                 <td>${caso['EXP CIVIL']}</td>
-                <td>${caso['CARPETA INVESTIGACIÓN']}</td>
+                <td>${caso['CARPETA INVESTIGACION']}</td>
                 <td>${caso['CARPETA JUDICIAL']}</td>
-                <td>${caso['PRETENSIÓN']}</td>
+                <td>${caso['PRETENSION']}</td>
                 <td>${caso['REPORTE']}</td>
                 <td>${caso['ACTIVO']}</td>
             `;
@@ -143,3 +141,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add event for search input
     searchInput.addEventListener('input', filterData);
 });
+
+
+
+               
